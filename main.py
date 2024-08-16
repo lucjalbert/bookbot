deeb39ed4f8c9fa23b6e63fe0f134ec2aca675ca
lucjalbert <lucjalbert01@gmail.com>
2024-08-16 02:23:38 -0400

    add individual letter counts and format output

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = input("Enter the path to a book to analyse: ")
     book = get_book_text(book_path)
     word_count = count_words(book)
-    character_count = count_characters(book)
-    print(character_count)
+    character_amount = count_characters(book)
+    sorted_characters = dict_to_list(character_amount)
+
+    print(f"--- Analysis of book: {book_path} ---")
     print(f" Your book contains: {word_count} words.")
 
+    for key, count in sorted_characters:
+        print(f"The letter '{key}' was found {count} times")
+    
+    print(f"--- Analysis successful ---")
 
-def get_book_text(path):   
-    with open(path) as f:
-        return f.read()
+
+def get_book_text(path):
+    try:
+        with open(path) as f:
+            return f.read()
+    except FileNotFoundError as e:
+        print("File path incorrect or missing")
+        sys.exit()
 
 
 def count_words(book):
-    book_words = book.split()
-    return len(book_words)
+    words = book.split()
+    return len(words)
 
 
 def count_characters(book):
-    book_lowercase = book.lower()
+    book_lowered = book.lower()
     dict_char = {}
-    alphabet = [chr(i) for i in range(ord("a"), ord("z") + 1)]
-    for letter in alphabet:
-        amount = 0
-        for character in book_lowercase:
-            if character == letter:
-                amount += 1
-        dict_char[letter] = amount
+    for c in book_lowered:
+        if c.isalpha():
+            dict_char[c] = dict_char.get(c, 0) + 1
     return dict_char
+
+
+def dict_to_list(dict_char):
+    dict_sorted = sorted(dict_char.items(), key=lambda item: item[1], reverse=True)
+    return dict_sorted
 
 
 main()
