60cc2cd6cf081c9f89dedd5f1b8f0d4a73280df0
lucjalbert <lucjalbert01@gmail.com>
2024-08-15 20:25:49 -0400

    add character count feature

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    word_count = count_words(text)
+    book = get_book_text(book_path)
+    word_count = count_words(book)
+    character_count = count_characters(book)
+    print(character_count)
     print(f" Your book contains: {word_count} words.")
 
 
@@ -13,6 +15,19 @@ def get_book_text(path):
 def count_words(book):
     book_words = book.split()
     return len(book_words)
+
+
+def count_characters(book):
+    book_lowercase = book.lower()
+    dict_char = {}
+    alphabet = [chr(i) for i in range(ord("a"), ord("z") + 1)]
+    for letter in alphabet:
+        amount = 0
+        for character in book_lowercase:
+            if character == letter:
+                amount += 1
+        dict_char[letter] = amount
+    return dict_char
 
 
 main()
